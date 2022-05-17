9fe816f058df3b7f7d83bcaabff51d55e6973b3b
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-05-18 01:37:49 +0200

    created database

---
+++ BookWormWeb/Data/ApplicationDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWormWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWormWeb.Data
+{
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BookWormWeb/Program.cs
+++ BookWormWeb/Program.cs
@@ -1,7 +1,13 @@
+using BookWormWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDatabaseContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
