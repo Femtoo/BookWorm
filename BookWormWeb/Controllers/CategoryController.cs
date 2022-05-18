dec4459f703d31d3b4e12095bf32418b3b20da18
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-05-18 23:28:21 +0200

    Dodanie category controllera, view Rozpoczęcie pracy nad buttonem create

---
+++ BookWormWeb/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using BookWormWeb.Data;
+using BookWormWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWormWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDatabaseContext _db;
+
+        public CategoryController(ApplicationDatabaseContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //get
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
