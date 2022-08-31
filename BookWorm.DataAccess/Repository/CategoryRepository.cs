3819f90fd9bbbafaa4eb4be94c974949622e41f9
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-08-31 21:03:29 +0200

    added ICategoryRepository and implemented CategoryRepository

---
+++ BookWorm.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookWorm.DataAccess.Repository.IRepository;
+using BookWorm.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWorm.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDatabaseContext _db;       
+        public CategoryRepository(ApplicationDatabaseContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
