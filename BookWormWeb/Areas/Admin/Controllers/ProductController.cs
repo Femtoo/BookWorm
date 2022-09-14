167337066938df5ee5263844bd58cada27839e44
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-09-14 14:47:20 +0200

    added index and edit(upsert) views for product

---
+++ BookWormWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using BookWorm.DataAccess;
+using BookWorm.DataAccess.Repository.IRepository;
+using BookWorm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookWormWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.ProductRepository.GetAll();
+            return View(objProductList);
+        }
+
+        //get
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(
+                u=> new SelectListItem
+                {
+                    Text=u.Name,
+                    Value=u.Id.ToString(),
+                }
+            );
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverTypeRepository.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }
+            );
+            if (id == null || id == 0)
+            {   
+                //create product
+                return View(product);
+            } 
+            else
+            {
+                //update product
+            }
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.ProductRepository.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.ProductRepository.GetFirstOrDefault(u => u.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        //post
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var productFromDb = _unitOfWork.ProductRepository.GetFirstOrDefault(u => u.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.ProductRepository.Remove(productFromDb);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
