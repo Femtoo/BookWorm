f314a7c9701597fe28d801587f1d7269e9f2ab21
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-09-28 14:29:55 +0200

    added company repository

---
+++ BookWorm.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookWorm.DataAccess.Repository.IRepository;
+using BookWorm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWorm.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDatabaseContext _db;
+        public CompanyRepository(ApplicationDatabaseContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            var objFromDb = _db.Companies.FirstOrDefault(u => u.Id == obj.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.StreetAddress = obj.StreetAddress;
+                objFromDb.PhoneNumber = obj.PhoneNumber;
+                objFromDb.City = obj.City;
+                objFromDb.PostalCode = obj.PostalCode;
+                objFromDb.State = obj.State;
+            }
+        }
+    }
+}
